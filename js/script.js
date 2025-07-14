37ce3cbf948b1b605ffb956f773e450a15b87d84
Denise Lescano <136858087+DeniLescano@users.noreply.github.com>
2025-07-13 22:08:06 -0300

    fin contador

--- js/script.js
+++ js/script.js
@@ -45,12 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
     const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
-    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
     document.getElementById("dias").textContent = dias;
     document.getElementById("horas").textContent = horas;
     document.getElementById("minutos").textContent = minutos;
-    document.getElementById("segundos").textContent = segundos;
   }
 
   const intervalo = setInterval(actualizarCuenta, 1000);
